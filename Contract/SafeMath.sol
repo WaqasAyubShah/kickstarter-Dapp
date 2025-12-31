c6006590768981482e332f11994e935c177d1f28
WaqasAyubShah <smwaqas.ayubshah@gmail.com>
2025-12-31 19:23:15 +0500

    Gass efficient

--- Contract/SafeMath.sol
+++ Contract/SafeMath.sol
@@ -3,10 +3,12 @@
 pragma solidity >=0.6.0 ;
 
 contract SafeMath{
-uint8 public Bignumber = 255;
+uint8 public Bignumber = 255; //checked
 
     function add() public {
         Bignumber = Bignumber + 1;
     }
     //after solidity 0.8 we have SafeMath but with checked and unchecked...
+    //why we will use unchecked, because it is gas efficient  if you are sure that we will not cross 255 then we can
+    //use it.. 
 }
