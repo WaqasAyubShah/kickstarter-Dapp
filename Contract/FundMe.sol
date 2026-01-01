9010066d805edf8e392e2dec2ad69dae463895e1
WaqasAyubShah <smwaqas.ayubshah@gmail.com>
2026-01-01 13:04:46 +0500

    zksync update

--- Contract/FundMe.sol
+++ Contract/FundMe.sol
@@ -5,8 +5,41 @@ pragma solidity >=0.7.0 <0.9.0;
 error Unauthorized();
 
 //Also we can import the AggregartorV3Interface from chainlink repo
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+//import {PriceConvertor} from "./PriceConvertor.sol";
+library PriceConvertor{
+    
+    //Check price feed on chainlink for this
+    function getPrice() public view returns (uint256) {
+        //price feed contract hash: https://docs.chain.link/data-feeds/price-feeds/addresses?networkType=testnet&testnetPage=2 
+        //Address for sapoliea 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //Address for zkSync: 0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF
+        // we can work around abi by using interfaces.
+        //ABI 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF);
 
-import {PriceConvertor} from "./PriceConvertor.sol";
+        (
+            /* uint80 roundID */,
+            int256 answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+       // (,int256 price,,,) = priceFeed.latestRoundData;
+        //price of ETH in usdt
+        //2000,00,000,000 covert it to 18 dec...
+        return uint256(answer * 1e10);
+    }
+    function getConversionRate(uint256 ethAmount) public view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmouninUSD = (ethAmount * ethPrice)/1e18;
+        return ethAmouninUSD;
+    }
+
+    function getVersion() public view returns(uint256){
+        return AggregatorV3Interface(0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF).version();
+    }
+}
 //from where i got the interface, if you are cursious about it
 //here is the link: https://github.com/smartcontractkit/smart-contract-examples/blob/630d83e9be2ee27625b13986c8d9938c4523397d/pricefeed-golang/aggregatorv3/AggregatorV3Interface.sol
 //Although you can just search aggregatorV3Interface in chainlink repo and it will work.
